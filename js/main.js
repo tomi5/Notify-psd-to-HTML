1422c549e2d43669c287402dd4b5e98beb9ace37
tomi5 <tomasz.rysak@o2.pl>
2019-08-14 15:13:42 +0200

    add carousell to iphone img in header, change default interval of carousel in main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
+// header carousel interval
+
+$('.carousel').carousel({
+    interval: 5000
+})
+
+
 // email validation for newsletter submit
 
 $('#newsletter').on('submit', function (e) {
- e.preventDefault();
- if ($('#newsletter')[0].checkValidity() === false || $('#email').val() === "") {
-  e.preventDefault();
-  e.stopPropagation()
- } else {
-  $('#confirm').modal('show')
- }
- $('#newsletter').addClass('was-validated')
+    e.preventDefault();
+    if ($('#newsletter')[0].checkValidity() === false || $('#email').val() === "") {
+        e.preventDefault();
+        e.stopPropagation()
+    } else {
+        $('#confirm').modal('show')
+    }
+    $('#newsletter').addClass('was-validated')
 })
